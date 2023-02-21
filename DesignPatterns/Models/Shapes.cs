97e196e0e7cec2ea8d08258e10f547ecf866ed39
Brandon Lewis <blewis@adorapos.com>
2023-02-21 14:16:59 -0800

    Liskov Substitution Principle

---
+++ DesignPatterns/Models/Shapes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Models
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Width: {Width}, Height: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+
+        public Square(int sideLength) : base(sideLength, sideLength)
+        {
+
+        }
+       public override int Width
+        {
+            set { base.Width = base.Height = value; }
+
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Square Width: {Width} - Square Height: {Height}";
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -4,54 +4,17 @@ using DesignPatterns.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Console;
-var j = new Journal();
-j.AddEntry("I cried today.");
-j.AddEntry("I ate a bug.");
-Console.WriteLine(j);
-var p = new Persistence();
-// @ sign allows you to use backslashes in a string and reads it as verbatim
-var filename = @"C:\Users\BrandonLewis\source\repos\journal.txt";
-p.SaveToFile(j, filename, true);
 
-// Open/Closed Principle
-Product[] products =
-{
-    new Product("Apple", Color.Green, Size.Small),
-    new Product("Tree", Color.Green, Size.Large),
-    new Product("House", Color.Blue, Size.Large)
-};
+static int Area(Rectangle r) => r.Width * r.Height;
+Rectangle rc = new Rectangle(4,8);
+WriteLine(Area(rc));
 
-var pf = new ProductFilter();
-WriteLine("Green products (old):");
-foreach (var product in pf.FilterByColor(products, Color.Green))
-{
-    WriteLine($" - {product.Name} is green");
-}
+WriteLine(rc.ToString());
 
-WriteLine("Filter By Size");
-foreach (var product in pf.FilterBySize(products, Size.Large))
-{
-    WriteLine($" - {product.Name} is large");
-}
+Rectangle sq = new Square(3);
 
-WriteLine("Filter By Color via Open Closed Principle");
-var bf = new BetterFilter();
-foreach (var product in bf.Filter(products, new ColorSpecification(Color.Green)))
-{
-    WriteLine($" - {product.Name} is green");
-}
-
-WriteLine("Filter By Size and Color via Open Closed Principle");
-IEnumerable<ISpecification<Product>> Specifications = new List<ISpecification<Product>>()
-{
-    new ColorSpecification(Color.Green),
-    new SizeSpecification(Size.Small),
-    new NameSpecification("Apple")
-};
-foreach (var product in bf.Filter(products, new AndSpecification(Specifications)))
-{
-    WriteLine($" - {product.Name} is an apple and green and large");
-}
+WriteLine(sq);
