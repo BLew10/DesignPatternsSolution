05be9a6e02209470d07f80303e16801474a95f46
Brandon Lewis <blewis@adorapos.com>
2023-02-21 11:45:21 -0800

    Add project files.

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+var j = new Journal();
+j.AddEntry("I cried today.");
+j.AddEntry("I ate a bug.");
+Console.WriteLine(j);
+var p = new Persistence();
+// @ sign allows you to use backslashes in a string and reads it as verbatim
+var filename = @"C:\Users\BrandonLewis\source\repos\journal.txt";
+p.SaveToFile(j, filename, true);
